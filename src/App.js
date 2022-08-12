dd400f8cfb8992300c3232cba4631ffbfb99546e
Vinu Vijayan <vinu.vijayan@evalogical.in>
2022-08-13 00:18:32 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleCredentialResponse = (response) =>{
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +23,13 @@ function App() {
           Learn React
         </a>
       </header>
+          <div id="g_id_onload"
+            data-client_id="772838587090-gps7g9rk76qvkrt3nu2s1jqqgg93qrj1.apps.googleusercontent.com"
+            data-login_uri="https://localhost:3000/"
+            data-cancel_on_tap_outside="false"
+            data-callback="handleCredentialResponse"
+            data-context="signin">
+          </div>
     </div>
   );
 }
